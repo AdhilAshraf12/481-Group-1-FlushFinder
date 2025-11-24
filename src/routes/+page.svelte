48170163e4b22509f3b4996785e3796e34809d62
darkicewolf50 <brock.tomlinson@ucalgary.ca>
2025-11-24 15:38:27 -0700

    feat(in app): some pages for once past the log in screen available

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-
+<a href="/inapp/find">to find</a>
 <div>
 	<h1>Sign up</h1>
 	<form action="">
---
+++ src/routes/inapp/+layout.svelte
@@ -0,0 +1,49 @@
+<script>
+	let { children } = $props();
+</script>
+
+{@render children?.()}
+<nav>
+	<a href="/find"
+		><img src="https://www.svgrepo.com/show/529715/map-point-wave.svg" alt="Pin Icon" /><span
+			>Find</span
+		></a
+	>
+
+	<a href="/inapp/review"><img src="" alt="Review Icon" /><span>Review</span></a>
+	<a href="/inapp/profile"><img src="" alt="Profile Icon" /><span>Profile</span></a>
+</nav>
+
+<style>
+	span {
+		display: block;
+	}
+	img {
+		width: 40px;
+		aspect-ratio: 1;
+		padding: 1svb 6svw;
+	}
+
+	a {
+		display: flex;
+		flex-direction: column;
+		text-align: center;
+		text-decoration: none;
+		cursor: pointer;
+		justify-content: center;
+	}
+
+	nav {
+		display: flex;
+		background-color: lightblue;
+		border-top-left-radius: 4svb;
+		border-top-right-radius: 4svb;
+		padding: 1svb;
+		justify-content: center;
+		justify-content: space-evenly;
+	}
+
+	.selected {
+		background-color: white;
+	}
+</style>
