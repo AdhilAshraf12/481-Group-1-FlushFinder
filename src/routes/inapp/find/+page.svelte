080d120ced638b7d11ef71430e47c795cd96cadb
darkicewolf50 <brock.tomlinson@ucalgary.ca>
2025-11-24 15:51:22 -0700

    feat(finder): started on search menu

---
+++ src/routes/inapp/find/+page.svelte
@@ -0,0 +1,18 @@
+<form action={console.log('Hello')}>
+	<div>
+		<button type="menu"
+			><img src="https://www.svgrepo.com/show/524617/hamburger-menu.svg" alt="Hamburger" /></button
+		>
+		<input type="text" value="Search for bathrooms" />
+		<button type="submit">
+			<img src="https://www.svgrepo.com/show/532552/search-alt-2.svg" alt="Search Icon" />
+		</button>
+	</div>
+</form>
+
+<style>
+	img {
+		width: 30px;
+		aspect-ratio: 1;
+	}
+</style>
